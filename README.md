6a7946364760d5e027e97066fe59b5161df149f1
ben <2388697970@qq.com>
2018-03-24 22:50:17 +0800

    fix some bug / uptate readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# console.css
+> console.css() can print text with css
+
+```
+console.css([{
+  text: 'request',
+  style: {
+    color: '#fff',
+    background: '#555',
+    borderRadius: '3px 0 0 3px',
+    padding: '2px 6px',
+    fontSize: '8px'
+  }
+}, {
+  text: 'successful',
+  style: {
+    color: '#fff',
+    background: '#00a698',
+    borderRadius: '0 3px 3px 0',
+    padding: '2px 6px',
+    fontSize: '8px'
+  }
+}])
+```
+![./images/1.png](./images/1.png)
--- index.js
+++ index.js
@@ -1,43 +1,3 @@
-/***************
-console.css([{
-  text: 'request',
-  style: {
-    color: '#fff',
-    background: '#555',
-    borderRadius: '3px 0 0 3px',
-    padding: '2px 6px',
-    fontSize: '8px'
-  }
-}, {
-  text: 'successful',
-  style: {
-    color: '#fff',
-    background: '#00a698',
-    borderRadius: '0 3px 3px 0',
-    padding: '2px 6px',
-    fontSize: '8px'
-  }
-}])
-console.css([{
-  text: 'request',
-  style: {
-    color: '#fff',
-    background: '#555',
-    borderRadius: '3px 0 0 3px',
-    padding: '2px 6px',
-    fontSize: '8px'
-  }
-}, {
-  text: 'error',
-  style: {
-    color: '#fff',
-    background: 'red',
-    borderRadius: '0 3px 3px 0',
-    padding: '2px 6px',
-    fontSize: '8px'
-  }
-}])
- ***************/
 void(function (console) {
   if (console === null ||
     console === void(0) ||
@@ -61,15 +21,16 @@ void(function (console) {
 
   function consoleCss(options) {
     if (typeof options !== 'object' || options === null) {
-      return console.log(Array.prototype.slice.call(arguments))
+      return console.log.apply(null, arguments)
     }
     var content = [];
     var opts = Array.isArray(options) ? options : [options];
     var styles = opts.map(function (item) {
       content.push(item.text);
-      return Object.keys(item.style).map(joinStyle.call(null, item)).join(';')
+      return Object.keys(item.style || {}).map(joinStyle.call(null, item)).join(';')
     })
-    console.log('%c' + content.join('%c'), ...styles);
+    console.log(styles)
+    console.log.apply(null, ['%c' + content.join('%c')].concat(styles));
   }
   consoleCss.toString = function () { return 'function css () { [native code] }'; };
   console['css'] = consoleCss;
